99fb1e9461bd07dd436837a7a74ed39820e43029
Lê Anh Vũ <135134776+AnhVu22@users.noreply.github.com>
2023-10-26 16:27:31 +0700

    up

--- C0723L1_LeAnhVu_Module2/src/ss02_loop_array/PrintShape.java
+++ C0723L1_LeAnhVu_Module2/src/ss02_loop_array/PrintShape.java
@@ -6,6 +6,15 @@ public class PrintShape {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int choice = -1;
-        System.out.println();
+        System.out.println("Menu");
+        System.out.println("Draw the rectangle");
+        System.out.println("Draw the right triangle - bottom.left");
+        System.out.println("Draw the right triangle - top.left");
+        choice = input.nextInt();
+    }
+    switch (choice) {
+        case 1:
+            System.out.println("Draw the rectangle");
+            
     }
 }
