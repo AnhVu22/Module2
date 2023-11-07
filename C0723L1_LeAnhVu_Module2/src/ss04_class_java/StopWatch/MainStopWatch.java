b409f2fbf9eb934a3251aed95c72b8be2741301b
Lê Anh Vũ <135134776+AnhVu22@users.noreply.github.com>
2023-11-07 11:39:50 +0700

    up

---
+++ C0723L1_LeAnhVu_Module2/src/ss04_class_java/StopWatch/MainStopWatch.java
@@ -0,0 +1,7 @@
+package ss04_class_java.StopWatch;
+
+public class MainStopWatch {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ C0723L1_LeAnhVu_Module2/src/ss04_class_java/StopWatch/StopWatch.java
@@ -0,0 +1,25 @@
+package ss04_class_java.StopWatch;
+
+public class StopWatch {
+    private long startTime;
+    private long endTime;
+    long getStartTime() {
+        return this.startTime;
+    }
+    long getEndTime() {
+        return this.endTime;
+    }
+    long setStartTime() {
+        this.startTime = System.currentTimeMillis();
+        return this.startTime;
+    }
+    void start() {
+        this.startTime = System.currentTimeMillis();
+    }
+    void end() {
+        this.endTime = System.currentTimeMillis();
+    }
+    long getElapsedTime() {
+        return this.endTime - this.startTime;
+    }
+}
