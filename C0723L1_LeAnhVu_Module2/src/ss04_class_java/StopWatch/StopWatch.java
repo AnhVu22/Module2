8b0343fa1f34d2b90f741c65a6bd6fc0388208fc
AnhVu22 <anhvucnmt@gmail.com>
2023-11-14 15:12:02 +0700

    up

--- C0723L1_LeAnhVu_Module2/src/ss04_class_java/StopWatch/StopWatch.java
+++ C0723L1_LeAnhVu_Module2/src/ss04_class_java/StopWatch/StopWatch.java
@@ -3,15 +3,16 @@ package ss04_class_java.StopWatch;
 public class StopWatch {
     private long startTime;
     private long endTime;
+
     long getStartTime() {
         return this.startTime;
-    }
-    long getEndTime() {
-        return this.endTime;
     }
     long setStartTime() {
         this.startTime = System.currentTimeMillis();
         return this.startTime;
+    }
+    long getEndTime() {
+        return this.endTime;
     }
     void start() {
         this.startTime = System.currentTimeMillis();
