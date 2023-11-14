7b98ad6bca0a5ea19e1a74d793c0c036e0d9a24e
Lê Anh Vũ <135134776+AnhVu22@users.noreply.github.com>
2023-11-14 14:21:45 +0700

    up

---
+++ C0723L1_LeAnhVu_Module2/src/ss07_interface/Colorable/IColorable.java
@@ -0,0 +1,5 @@
+package ss07_interface.Colorable;
+
+public interface IColorable {
+    void howToColor();
+}
---
+++ C0723L1_LeAnhVu_Module2/src/ss07_interface/Resizeable/IResizeable.java
@@ -0,0 +1,5 @@
+package ss07_interface.Resizeable;
+
+public interface IResizeable {
+    void resize(double percent);
+}
