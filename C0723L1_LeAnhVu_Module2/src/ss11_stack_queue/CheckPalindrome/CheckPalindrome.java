b18e5d8ba151a4993b4ef0b389baf67628e06327
Lê Anh Vũ <135134776+AnhVu22@users.noreply.github.com>
2023-12-06 18:29:20 +0700

    up

--- C0723L1_LeAnhVu_Module2/src/ss11_stack_queue/CheckPalindrome/CheckPalindrome.java
+++ C0723L1_LeAnhVu_Module2/src/ss11_stack_queue/CheckPalindrome/CheckPalindrome.java
@@ -1,11 +1,13 @@
 package ss11_stack_queue.CheckPalindrome;
 
 import com.sun.jmx.remote.internal.ArrayQueue;
+
+import java.util.ArrayDeque;
 import java.util.Queue;
 
 public class CheckPalindrome {
     public boolean isPalindrome(String str) {
-        Queue<Character> stringQueue = new ArrayQueue<>();
+        Queue<Character> stringQueue = new ArrayDeque<>();
         String upper = str.toUpperCase();
         for (int i = upper.length() - 1; i >= 0; i--) {
             stringQueue.add(upper.charAt(i));
